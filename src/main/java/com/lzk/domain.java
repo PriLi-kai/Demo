3d04ac2214a47319f559f8203f522e204e862401
PriLi-kai <836669099@qq.com>
2019-07-27 18:44:38 +0800

    第一次使用

---
+++ src/main/java/com/lzk/domain.java
@@ -0,0 +1,14 @@
+package com.lzk;
+
+import javax.naming.Name;
+
+public class domain {
+    private static String name;
+    private static int age;
+
+    public static void main(String[] args) {
+        name = "李振凯";
+        age=18;
+        System.out.println(name+"的年龄为"+age);
+    }
+}
